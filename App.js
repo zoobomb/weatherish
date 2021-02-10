60620c2dfa76d9c3c5caa695059ac7730ede3c0e
Hajoo Chung <hajoo.chung@gmail.com>
2021-02-10 00:13:19 +0000

    get current location with expo

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Alert } from "react-native";
+import Loading from "./Loading";
+import * as Location from "expo-location";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// export default function App() {
+//   return <Loading />;
+// }
+
+export default class extends React.Component {
+  getLocation = async () => {
+    try {
+      // throw Error();
+      await Location.requestPermissionsAsync();
+      const location = await Location.getCurrentPositionAsync();
+      console.log(location);
+    } catch (error) {
+      Alert.alert("Can't find you", "😧");
+      //  console.log("hello");
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    return <Loading />;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Loading.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Loading() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Getting the Weather!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FDF6AA",
+    justifyContent: "flex-end",
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+    // alignItems: "center",
+  },
+  text: {
+    color: "grey",
+    fontSize: 30,
+  },
+});
